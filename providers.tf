7e495ee31ec7d417ca027689c1cc353dc06696ff
Vikas <tiwarivikash795@gmail.com>
2025-06-20 18:00:40 +0530

    added statelock table in providers.tf

--- providers.tf
+++ providers.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "terraform.tfstate"          # Path inside the bucket
     region         = "us-east-1"                  # AWS region
     encrypt        = true                         # Encrypt state file
+    dynamodb_table = "terraform-lock-table"  # <-- Added for state locking
   }
 }
